e9a6c6bb165743bfe089ec926ae1761d1dff6e0f
elemanhillary <elemanhillary@gmail.com>
2019-05-22 11:39:10 -0700

    [ft #165580432] adds verify user function

--- controllers/admin.js
+++ controllers/admin.js
@@ -25,3 +25,31 @@ export const getUsers = async (req, res) => {
     });
   }
 };
+
+export const verifyUser = async (req, res) => {
+  try {
+    const { rows } = await query(`UPDATE users SET 
+    status = 'verified' where email = $1 returning *`, [req.params.email]);
+    if (rows.length > 0 && rows[0].status === 'verified') {
+      res.status(200).json({
+        status: 200,
+        message: rows[0].status,
+      });
+    } else if (rows.length > 0 && rows[0].status === 'unverified') {
+      res.status(304).json({
+        status: 304,
+        message: 'unverified',
+      });
+    } else {
+      res.status(204).json({
+        status: 204,
+        message: 'user doesnot exist',
+      });
+    }
+  } catch (e) {
+    res.status(400).json({
+      status: 400,
+      message: e.detail,
+    });
+  }
+};
